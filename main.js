7641e1150f8340f87cab647854cca10e2587e1e2
javdev <javier.viera007@gmail.com>
2022-10-05 16:13:51 -0300

    segunda entrega

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+//PRIMER ENTREGA
 
-function agarrarNumero(){
+
+/* function agarrarNumero(){
     const numero = Number(prompt("ingresa un numero "));
     if(numero===0){
         return 10;
@@ -25,5 +27,34 @@ for(let i=1; i<=numero; i++){
     }
 
 }
+ */
 
+
+ //SEGUNDA ENTREGA
+
+
+ let personajes = []
+
+
+ function pjs(name, status, specie){
+     this.name = name;
+     this.status = status;
+     this.specie = specie;
+ }
  
+ 
+ personajes.push (new pjs("Rick Sanchez", "alive", "human"))
+ personajes.push (new pjs("Alien Morty", "alive", "alien"))
+ personajes.push (new pjs("Adjudicator Sanchez", "dead", "human"))
+ personajes.push (new pjs("Summer Smith", "alive", "human"))
+ personajes.push (new pjs("Beth Smith", "alive", "human"))
+ personajes.push (new pjs("Alan Rails", "dead", "human"))
+ 
+ 
+ for(let item of personajes){
+     console.log(item.name)
+ }
+ 
+ 
+ let muertos = personajes.filter(persona => persona.status === "dead")
+ console.log(muertos)
